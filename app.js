b4bef49b7cf13c636595da8b2fbffec42d3aa3d1
Jeffrey Mills <jeff@millsmultimedia.net>
2018-04-28 17:22:12 -0500

    final tutorial code

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+//Use session middleware
+app.use(session({
+	secret: 'session-tutorial',
+	resave: true,
+	saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
+
+//users list (normally this would be in your database and a service class)
+var userList=[{_id:1,firstName:'Steve',lastName:'Rogers',email:'captainamerica@avengers.com',website:'http://www.millsmultimedia.net',password:'password',accountLevel:'user'},{_id:2,firstName:'Tony',lastName:'Stark',email:'ironman@avengers.com',website:'http://www.millsmultimedia.net',password:'password',accountLevel:'user'},{_id:3,firstName:'Natasha',lastName:'Romanova',email:'blackwidow@avengers.com',website:'http://www.millsmultimedia.net',password:'password',accountLevel:'user'},{_id:4,firstName:'Bruce',lastName:'Banner',email:'thehulk@avengers.com',website:'http://www.millsmultimedia.net',password:'password',accountLevel:'user'},{_id:5,firstName:'Clint',lastName:'Barton',email:'hawkeye@avengers.com',website:'http://www.millsmultimedia.net',password:'password',accountLevel:'user'},{_id:6,firstName:'Thor',lastName:'Odinson',email:'thor@avengers.com',website:'http://www.millsmultimedia.net',password:'password',accountLevel:'user'}];
 
 /* redirect to login page */
 router.get('/', function(req, res, next) {
-  res.redirect('/login');
+  res.send(req.session.id);
 });
 
 /* GET login page. */
@@ -11,22 +15,52 @@ router.get('/login', function(req, res, next) {
 	res.render('login');
 });
 
+
+
 /* GET user-login route. */
 router.get('/user-login', function(req, res, next) {
-	res.redirect('/login');
+	//if the user is already logged in
+	if (req.session.user)
+		res.redirect('/user-landing/' + req.session.user._id)
+	else {
+		//see if user exists
+		var loggedInUser = userList.find( user => user.email == req.param('username') );
+
+		//see if the password matches the user
+		if (loggedInUser != undefined && loggedInUser.password == req.param('password') ) {
+			//add the user as a session variable
+			req.session.user = loggedInUser;
+
+			//then redirect to the user's landing page
+			res.redirect('/user-landing/' + req.session.user._id)
+		}
+		else {
+
+			res.render('login', {wrongLogin: true});
+		}
+	}
 });
 
-router.get('/user-landing', function(req, res, next) {
-	res.render('user-landing', {firstname: 'testing'});
+
+
+router.get('/user-landing/:userid', function(req, res, next) {
+	if (req.session.user && req.session.user._id == req.params.userid)
+		res.render('user-landing', {user: req.session.user});
+	else
+		res.render('login', {loginrequired: true});
 });
 
 /* GET user-login route. */
-router.get('/user-info', function(req, res, next) {
-	res.render('user-info', {firstname: 'testing'});
+router.get('/user-info/:userid', function(req, res, next) {
+	if (req.session.user && req.session.user._id == req.params.userid)
+		res.render('user-info', {user: req.session.user});
+	else
+		res.render('login', {loginrequired: true});
 });
 
 /* GET user-login route. */
 router.get('/logout', function(req, res, next) {
+	req.session.destroy();
 	res.redirect('/login');
 });
 
